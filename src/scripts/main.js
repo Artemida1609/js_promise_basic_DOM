4c7fbdd6c59e224e6fbc7f66d0eb0fe4d0a84ae8
Artem <skickoartem74@gmail.com>
2025-02-04 11:46:24 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+const success = document.createElement('div');
+
+success.className = 'message';
+
+const error = document.createElement('div');
+
+error.className = 'error-message';
+
+logo.style.cursor = 'pointer';
+
+logo.addEventListener('click', () => {
+  Promise.resolve(`Promise was resolved!`).then((message) => {
+    const textNode = document.createTextNode(message);
+
+    success.appendChild(textNode);
+  });
+});
+
+setTimeout(() => {
+  Promise.reject(new Error('Promise was rejected!')).catch((message) => {
+    const textNode = document.createTextNode(message);
+
+    error.appendChild(textNode);
+  });
+}, 3.0 * 1000);
