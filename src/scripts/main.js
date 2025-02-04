f896fd39d943b7b8afac498b648c2ca832cda1d6
Artem <skickoartem74@gmail.com>
2025-02-04 12:23:02 +0100

    final solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,11 @@
 
 // write your code here
 const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
 
 const success = document.createElement('div');
 
 success.className = 'message';
-
-const error = document.createElement('div');
-
-error.className = 'error-message';
 
 logo.style.cursor = 'pointer';
 
@@ -18,13 +15,19 @@ logo.addEventListener('click', () => {
     const textNode = document.createTextNode(message);
 
     success.appendChild(textNode);
+    body.appendChild(success);
   });
 });
 
 setTimeout(() => {
-  Promise.reject(new Error('Promise was rejected!')).catch((message) => {
-    const textNode = document.createTextNode(message);
+  Promise.reject(new Error('Promise was rejected!')).catch((error) => {
+    const textNode = document.createTextNode(error.message);
 
-    error.appendChild(textNode);
+    const errorElement = document.createElement('div');
+
+    error.className = 'error-message';
+
+    errorElement.appendChild(textNode);
+    body.appendChild(errorElement);
   });
 }, 3.0 * 1000);
